de79d4d6a86bd570dd8ea6592aad620990ef2dc4
Cristina <cristinamateospaez@gmail.com>
2025-05-18 20:13:02 +0200

    Refactor CultivoRepository to implement JpaRepository for database operations

--- src/main/java/com/farm/farm/repository/CultivoRepository.java
+++ src/main/java/com/farm/farm/repository/CultivoRepository.java
@@ -1,3 +1,8 @@
-public class CultivoRepository {
+package com.farm.farm.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farm.farm.model.Cultivo;
+
+public interface CultivoRepository extends JpaRepository<Cultivo, Long> {
 }
