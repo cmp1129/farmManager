5708aba4517c0950ebbe91f46113a8335785cb22
Cristina <cristinamateospaez@gmail.com>
2025-05-29 08:32:57 +0200

    Add new image asset and clean up EmpleadoController formatting

--- src/main/java/com/farm/farm/controller/EmpleadoController.java
+++ src/main/java/com/farm/farm/controller/EmpleadoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmpleadoController {
-    
+
     @GetMapping("/empleados/listado")
     public String listarTodos(Model model) {
         return "mantenimiento";
